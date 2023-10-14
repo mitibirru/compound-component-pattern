009ae9bc4892dd40818e3b6f6a8c5c3a67e8f3c9
Mitesh Birru <79537705+mitibirru@users.noreply.github.com>
2023-10-14 19:35:23 +0530

    final ccp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./Counter";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div>
+			<h1>Compound Component Pattern</h1>
+			{/* <Counter
+				iconIncrease="+"
+				iconDecrease="-"
+				label="My NOT so flexible counter"
+				hideLabel={false}
+				hideIncrease={false}
+				hideDecrease={false}
+			/> */}
+
+			<Counter>
+				<Counter.Label>My super flexible counter</Counter.Label>
+				<Counter.Increase icon="+" />
+				<Counter.Decrease icon="-" />
+				<Counter.Count />
+			</Counter>
+			<div>
+				<Counter>
+					<Counter.Decrease icon="◀️" />
+					<Counter.Count />
+					<Counter.Increase icon="▶️" />
+				</Counter>
+			</div>
+		</div>
+	);
 }
-
-export default App;
